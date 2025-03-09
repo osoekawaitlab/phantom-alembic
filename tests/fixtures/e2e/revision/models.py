3ee6aa29a86a12898ac319ca56ae6b2441838f33
osoken <osoken@osoekawalab.com>
2025-03-09 17:52:03 +0900

    feat: add revision command

---
+++ tests/fixtures/e2e/revision/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
